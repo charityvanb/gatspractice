dec3a716a7ea8bb2f148b4b7ae0d904427fbb1d2
charityvanb <charityvanb@gmail.com>
2019-03-03 18:36:37 -0700

    Made the thing go properly

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,8 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-helmet`, `gatsby-transformer-sharp`, `gatsby-plugin-sharp`,
+    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
